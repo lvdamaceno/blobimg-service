f88805faf0bd642a9bdb194241a34214da26c2d1
Luís Vinícius Damaceno <lvdamaceno@gmail.com>
2025-05-04 19:52:37 -0300

    cache

--- main.py
+++ main.py
@@ -1,20 +1,19 @@
 from fastapi import FastAPI, Response, BackgroundTasks
 from sankhya_api.sk_api_utils import consulta_sankhya
 from pathlib import Path
-import os
 
 app = FastAPI()
 IMAGES_DIR = Path("images")
 IMAGES_DIR.mkdir(exist_ok=True)  # Garante que a pasta existe
 
 
-def get_image(codprod: int) -> Path:
+def consultar_e_salvar_imagem(codprod: int) -> Path:
     sankhya_service = "DbExplorerSP.executeQuery"
     query = f"SELECT IMAGEM FROM TGFPRO WHERE CODPROD = {codprod}"
     result = consulta_sankhya(query, sankhya_service)
 
     if not result or not result[0]:
-        raise ValueError("Imagem não encontrada para o produto.")
+        raise ValueError("Imagem não encontrada no banco de dados.")
 
     imagem_hex = result[0]
     imagem_bytes = bytes.fromhex(imagem_hex)
@@ -28,16 +27,16 @@ def get_image(codprod: int) -> Path:
 
 @app.get("/{codprod}")
 @app.get("/{codprod}.jpg")
-async def serve_image(codprod: int, background_tasks: BackgroundTasks):
+async def serve_image(codprod: int):
+    image_path = IMAGES_DIR / f"{codprod}.jpg"
+
+    if image_path.exists():
+        # Servindo imagem do cache
+        return Response(content=image_path.read_bytes(), media_type="image/jpeg")
+
     try:
-        image_path = get_image(codprod)
+        # Buscar no banco e salvar
+        image_path = consultar_e_salvar_imagem(codprod)
+        return Response(content=image_path.read_bytes(), media_type="image/jpeg")
     except Exception as e:
         return Response(content=str(e), status_code=404)
-
-    def delete_file(path: Path):
-        if path.exists():
-            path.unlink()
-
-    background_tasks.add_task(delete_file, image_path)
-
-    return Response(content=image_path.read_bytes(), media_type="image/jpeg")
